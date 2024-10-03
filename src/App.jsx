2aa8259044c79385c1d7a2159fd031aa784f73d4
ObalaSax <120820140+ObalaSax@users.noreply.github.com>
2024-10-04 01:09:50 +0300

    W5 D4 End

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import NavigationBar from "./Components/NavigationBar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SideBar from "./Components/SideBar";
+import Dashboard from "./Pages/Dashboard";
+import Accounts from "./Pages/Accounts";
+import Transactions from "./Pages/Transactions";
+import Settings from "./Pages/Settings";
+import Support from "./Pages/Support";
 function App() {
   return (
-    <>
-      <NavigationBar />
+    <Router>
       <SideBar />
-    </>
+      <Routes>
+        <Route index path="/" element={<Dashboard />} />
+        <Route path="/accounts" element={<Accounts />} />
+        <Route path="/transactions" element={<Transactions />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/support" element={<Support />} />
+      </Routes>
+    </Router>
   );
 }
-
 export default App;
--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,8 +1,51 @@
-import { ShoppingBag } from "lucide-react";
+import { useState } from "react";
 
 function SideBar() {
+  const [menuOpen, setMenue] = useState(false);
+  const handleStuff = () => {
+    setMenue(!menuOpen);
+  };
   return (
-    <div> ShoppingBag</div>
+    <>
+      <div className={menuOpen === false ? "sidebar" : "sidebar active"}>
+        <div className="logo-container">
+          <img src="" alt="" className="logo" />
+          <h2 className="title"></h2>
+        </div>
+        <div className="burger-container">
+          <div className="burger-trigger"></div>
+          <div className="burger-menu"></div>
+        </div>
+        <div className="profile-container">
+          <img src="" alt="profile" className="profi" />
+          <div className="profile-content">
+            <p className="username"> Wagwan Fam</p>
+            <p className="mail">obalasax.com</p>
+          </div>
+        </div>
+        <div className="content-container">
+          <ul>
+            <li>
+              <a href="/">Dashboard</a>
+            </li>
+            <li>
+              <a href="/transactions">Transaction</a>
+            </li>
+            <li>
+              <a href="/accounts">Account</a>
+            </li>
+            <li>
+              <a href="/settings">Setting</a>
+            </li>
+            <li>
+              <a href="/support">Support</a>
+            </li>
+            <button onClick={handleStuff}>Buttooon</button>
+          </ul>
+        </div>
+      </div>
+    </>
   );
 }
+
 export default SideBar;
